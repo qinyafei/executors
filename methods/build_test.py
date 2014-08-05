574f581f233aa8461b63f52822f646ebec825b8d
jamie <jamie@debianbox>
2014-08-05 16:31:01 +0100

    Squashed 'site_scons/' content from commit 3e485b2
    git-subtree-dir: site_scons
    git-subtree-split: 3e485b226a0f9630473bdb85c7b657b5240129e6

---
+++ methods/build_test.py
@@ -0,0 +1,36 @@
+
+#          Copyright Jamie Allsop 2011-2014
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+#-------------------------------------------------------------------------------
+#   BuildTestMethod
+#-------------------------------------------------------------------------------
+
+
+class BuildTestMethod:
+
+    def __init__( self, toolchain, default_test_runner=None ):
+        self._toolchain = toolchain
+        self._default_test_runner = default_test_runner
+
+
+    def __call__( self, env, target, source, final_dir=None, data=None, append_variant=None, test_runner=None, expected='success' ):
+        program = env.Build( target, source, final_dir=final_dir, append_variant=append_variant )
+        if env['variant_actions'].has_key('test') or env['variant_actions'].has_key('cov'):
+            if not test_runner:
+                test_runner = self._default_test_runner
+
+            env.Test( program, final_dir=final_dir, data=data, test_runner=test_runner, expected=expected )
+            if 'cov' in env['variant_actions']:
+                env.Coverage( program, source, final_dir=final_dir )
+
+        return program
+
+
+    @classmethod
+    def add_to_env( cls, args ):
+        args['env'].AddMethod( cls( args['env']['toolchain'], args['env']['default_test_runner'] ), "BuildTest" )
+        for test_runner in args['env']['toolchain'].test_runners():
+            args['env'].AddMethod( cls( args['env']['toolchain'], test_runner ), "Build{}Test".format( test_runner.title() ) )
